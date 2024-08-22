12c57e8c44004188b0b92373bcbb2efba5158218
deepak_mandolia <deepak_mandolia>
2024-08-22 22:59:21 +0530

    extra line

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ export const index = () => {
     <div>index</div>
      <div>index</div>
       <div>index</div> 
-      <div>index</div>
+      <div>index</div> <div>index</div>
+
+
   </>
    
   )
